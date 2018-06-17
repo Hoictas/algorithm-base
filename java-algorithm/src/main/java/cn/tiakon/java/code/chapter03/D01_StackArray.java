1651d0bd03681e6b1700faab586c8662aef2f05b
Hoictas <3439066717@qq.com>
2018-06-17 18:37:41 +0800

    数据结构和算法中的栈

---
+++ java-algorithm/src/main/java/cn/tiakon/java/code/chapter03/D01_StackArray.java
@@ -0,0 +1,16 @@
+package cn.tiakon.java.code.chapter03;
+
+/**
+ * Desc :
+ * <p>
+ * 栈就是一组记录，表示形式为先进后出。
+ * 1.通常情况作为程序员的工具来运用
+ * 2.受限访问
+ * 3.更加抽象(主要通过接口实现)
+ * <p>
+ * User : By Tiakon
+ * Time : 2018/6/17 18:31
+ */
+public class D01_StackArray {
+    private int[] stackArray;
+}
